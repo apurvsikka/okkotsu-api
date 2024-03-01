373c7c1f828e6727b8b387b4fc344f39a9aa85b8
Kenimarru <kenimaru@skiff.com>
2024-03-01 15:15:43 +0100

    update: added popular endpoint and pagination system

--- src/modules.js
+++ src/modules.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 import { load } from "cheerio";
 
-export const getRecentEpisodes = async () => {
-    const url = "https://anitaku.to/home.html"
+const getPagination = async (body) => {
+    const $ = load(body);
+    const pagination = {
+        current: $(".pagination ul li.selected a").text() ? $(".pagination ul li.selected a").text() : null,
+        last: $(".pagination ul li:last").text()
+    }
+    return pagination
+}
+
+export const getRecentEpisodes = async (page) => {
+    const url = `https://anitaku.to/home.html?page=${page}`
     const data = []
     const body = (await axios.get(url)).data
     const $ = load(body);
@@ -15,5 +24,21 @@ export const getRecentEpisodes = async () => {
         }
         data.push(item);
     });
-    return data
+    return { pagination: await getPagination(body), data }
+}
+
+export const getPopular = async (page) => {
+    const url = `https://anitaku.to/popular.html?page=${page}`
+    const data = []
+    const body = (await axios.get(url)).data
+    const $ = load(body);
+    $(".last_episodes ul li").each((index, element) => {
+        const item = {
+            id: $(element).find("p.name a").attr("href").slice(10),
+            title: $(element).find(".name a").text(),
+            cover: $(element).find(".img img").attr("src")
+        }
+        data.push(item);
+    });
+    return { pagination: await getPagination(body), data }
 }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getRecentEpisodes } from "./modules.js";
+import { getRecentEpisodes, getPopular } from "./modules.js";
 export const router = Router();
 
 router.get("/recent", async (req, res) => {
@@ -7,7 +7,23 @@ router.get("/recent", async (req, res) => {
         res.status(200).json({
             code: 200,
             message: "success",
-            data: await getRecentEpisodes()
+            data: await getRecentEpisodes(req.query.page)
+        });
+    } catch (error) {
+        res.status(500).json({
+            code: 500,
+            type: error.name,
+            message: error.message
+        });
+    }
+});
+
+router.get("/popular", async (req, res) => {
+    try {
+        res.status(200).json({
+            code: 200,
+            message: "success",
+            data: await getPopular(req.query.page)
         });
     } catch (error) {
         res.status(500).json({
